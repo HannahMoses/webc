ced70e7ed21640cd0efb7bfb9c869723a31c0e94
Hannah Mungamuri <hannahmoses612@gmail.com>
2017-01-26 01:37:04 -0600

    added text area

--- main.py
+++ main.py
@@ -18,6 +18,48 @@ import webapp2
 import caesar
 class MainHandler(webapp2.RequestHandler):
     def get(self):
+        message = 'Iellooooooo world!'
+        encrypted_message = caesar.encrypt(message,13)
+        self.response.write("<textarea>"+ encrypted_message +"</textarea>")
+
+app = webapp2.WSGIApplication([
+    ('/', MainHandler)
+], debug=True)
+
+
+
+
+
+
+
+
+
+
+
+
+
+'''
+
+#!/usr/bin/env python
+#
+# Copyright 2007 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+import webapp2
+import caesar
+class MainHandler(webapp2.RequestHandler):
+    def get(self):
         message = "Hellooooooo world!"
         encrypted_message = caesar.encrypt(message,13)
         self.response.write(encrypted_message)
@@ -25,3 +67,4 @@ class MainHandler(webapp2.RequestHandler):
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
 ], debug=True)
+'''
